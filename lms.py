68250d7c0271ff885e419015b13e78d47d8400ba
Kyle <132386456+Kyledcm@users.noreply.github.com>
2024-11-18 20:06:20 +0800

    lms testing

--- lms.py
+++ lms.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 from datetime import datetime
 
 app = Flask(__name__)
-DB_HOST=os.getenv("DB_HOST"),
-DB_USER=os.getenv("DB_USER"),
-DB_PASSWORD=os.getenv("DB_PASSWORD"),
-DB_NAME=os.getenv("DB_NAME"),
-DB_PORT=os.getenv("DB_PORT"),
+DB_HOST=os.getenv('DB_HOST'),
+DB_USER=os.getenv('DB_USER'),
+DB_PASSWORD=os.getenv('DB_PASSWORD'),
+DB_NAME=os.getenv('DB_NAME'),
+DB_PORT=os.getenv('DB_PORT', 5432),
 sslmode='require'
 # Database connection
 def lmsdb():
